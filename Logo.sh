f8012e8a6bd41f6b6c1d309296a796d7285944e8
hiddenode <sarkalay.freedom@gmail.com>
2026-02-26 21:11:27 +0700

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define color codes
-INFO='\033[0;36m'  # Cyan
+INFO='\033[0;36m'   # Cyan
 BANNER='\033[0;35m' # Magenta
 YELLOW='\033[0;33m' # Yellow
 RED='\033[0;31m'    # Red
@@ -9,26 +9,29 @@ GREEN='\033[0;32m'  # Green
 BLUE='\033[0;34m'   # Blue
 NC='\033[0m'        # No Color
 
-# Display social details and channel information in large letters manually
-echo "========================================"
-echo -e "${YELLOW} Script is made by HIDDENODE${NC}"
-echo -e "-------------------------------------"
+echo -e "${BLUE}=======================================================${NC}"
+echo -e "${YELLOW}       Script is made by HIDDENODE${NC}"
+echo -e "${BLUE}-------------------------------------------------------${NC}"
 
+# Logo ကို BLUE အရောင်နဲ့ ပြောင်းထားပေးပါတယ်
+echo -e "${BLUE}"
 cat << EOF
-██╗  ██╗██╗██████╗ ██████╗ ███████╗███╗   ██╗ ██████╗ ██████╗ ███████╗
-██║  ██║██║██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔═══██╗██╔══██╗██╔════╝
-███████║██║██║  ██║██║  ██║█████╗  ██╔██╗ ██║██║   ██║██║  ██║█████╗  
-██╔══██║██║██║  ██║██║  ██║██╔══╝  ██║╚██╗██║██║   ██║██║  ██║██╔══╝  
-██║  ██║██║██████╔╝██████╔╝███████╗██║ ╚████║╚██████╔╝██████╔╝███████╗
-╚═╝  ╚═╝╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝
+ ██╗  ██╗██╗██████╗ ██████╗ ███████╗███╗   ██╗ ██████╗ ██████╗ ███████╗
+ ██║  ██║██║██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔═══██╗██╔══██╗██╔════╝
+ ███████║██║██║  ██║██║  ██║█████╗  ██╔██╗ ██║██║   ██║██║  ██║█████╗  
+ ██╔══██║██║██║  ██║██║  ██║██╔══╝  ██║╚██╗██║██║   ██║██║  ██║██╔══╝  
+ ██║  ██║██║██████╔╝██████╔╝███████╗██║ ╚████║╚██████╔╝██████╔╝███████╗
+ ╚═╝  ╚═╝╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝
 EOF
+echo -e "${NC}"
 
-echo -e "${RED} This script is exclusive for private members only. Do not share resources publicly.${NC}"
-echo -e "------------------------------------------------------------"
+echo -e "${RED} This script is exclusive for private members only.${NC}"
+echo -e "${RED} Do not share resources publicly.${NC}"
+echo -e "${BLUE}-------------------------------------------------------${NC}"
 echo -e "${YELLOW} Want to join private? Contact - @seamya ${NC}"
 
-echo "======================================================="
+echo -e "${BLUE}=======================================================${NC}"
 echo -e "${YELLOW}Facebook: ${GREEN}https://www.facebook.com/profile.php?id=100057383421011${NC}"
-echo -e "${YELLOW}YouTube: ${GREEN}https://www.youtube.com/@airdropbombnode${NC}"
-echo -e "${YELLOW}Medium: ${INFO}https://medium.com/@airdropbomb${NC}"
-echo "======================================================="
+echo -e "${YELLOW}YouTube:  ${GREEN}https://www.youtube.com/@hiddenode${NC}"
+echo -e "${YELLOW}Medium:   ${INFO}https://medium.com/@hiddenode${NC}"
+echo -e "${BLUE}=======================================================${NC}"
